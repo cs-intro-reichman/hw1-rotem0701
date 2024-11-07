6fe578944dd3634eec625599d0c4535cf4d4359b
Rotem Weintal <rotem.weintal@post.runi.ac.il>
2024-11-07 16:08:48 +0200

    Fix the names order

--- Bill3.java
+++ Bill3.java
@@ -5,7 +5,7 @@ public class Bill3 {
 	    String name2 = args[1];
 		String name3 = args[2];
 		int totalBill = Integer.parseInt(args[3]);
-		System.out.println("Dear " + name1 + ", " + name2 + ", and " + name3 + 
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + 
 		": pay " + Math.ceil((double) totalBill / 3) + " Shekels each.");		
 	}
 }
