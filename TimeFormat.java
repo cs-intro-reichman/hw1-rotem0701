587a89fc8f65e7e83e54a7de890568937e941817
Rotem Weintal <rotem.weintal@post.runi.ac.il>
2024-11-07 15:52:52 +0200

    Fix a bug in 12:00

--- TimeFormat.java
+++ TimeFormat.java
@@ -12,7 +12,7 @@ public class TimeFormat {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-		if (hours < 13) {
+		if (hours < 12) {
 			if (args[0].charAt(3) == '0') {
 				System.out.println(hours + ":0" + minutes + " AM");
 			} else {
@@ -20,7 +20,7 @@ public class TimeFormat {
 			}
 		}
  
-		if (hours >= 13) {
+		if (hours >= 12) {
 			if (args[0].charAt(3) == '0') {
 				System.out.println(hours + ":0" + minutes + " PM");
 			} else {
